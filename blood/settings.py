879aa904abfbdaa89ad78665b974b398fdbe78c1
Muhammed Iqbal MN <iqbalnishad137@gmail.com>
2026-09-02 22:44:35 +0530

    small

--- blood/settings.py
+++ blood/settings.py
@@ -129,9 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'Blood_Frontend',
-]
+STATICFILES_DIRS = []
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
