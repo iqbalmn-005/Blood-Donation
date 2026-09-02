20cfa4cc2a51a33900625e64fd84cd69a3c556d3
Muhammed Iqbal MN <iqbalnishad137@gmail.com>
2026-09-02 20:56:35 +0530

    Explicitly handle DATABASE_URL for Vercel deployment

--- blood/settings.py
+++ blood/settings.py
@@ -80,13 +80,26 @@ WSGI_APPLICATION = 'blood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    if os.getenv('VERCEL'):
+        raise RuntimeError("DATABASE_URL environment variable is not set in Vercel Environment Variables!")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
