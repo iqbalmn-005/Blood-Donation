1d2d0c246e2ab5b2b175f1e3b772fee7331fbd6c
Muhammed Iqbal MN <iqbalnishad137@gmail.com>
2026-09-02 21:00:47 +0530

    Remove SQLite and set Neon PostgreSQL as default database

--- blood/settings.py
+++ blood/settings.py
@@ -80,25 +80,17 @@ WSGI_APPLICATION = 'blood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv(
+            'DATABASE_URL',
+            'postgresql://neondb_owner:npg_D9czU3MPukvC@ep-lively-base-axw4gsvq-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require'
+        ),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
-    }
-else:
-    if os.getenv('VERCEL'):
-        raise RuntimeError("DATABASE_URL environment variable is not set in Vercel Environment Variables!")
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 
 
